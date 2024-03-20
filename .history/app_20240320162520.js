0c0c077467902e050539f6a554c1c25c4e143eee
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-03-20 17:02:58 +0800

    初始化
    初始化

---
+++ .history/app_20240320162520.js
@@ -0,0 +1,16 @@
+// 导入 express 模块
+const express = require('express');
+
+// 创建 express 应用
+const app = express();
+
+// 定义一个简单的 GET 路由路径
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// 监听 3000 端口
+const port = 3000;
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}`);
+});
